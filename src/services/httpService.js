818fed93e682e333a5e87fb615ae05605bcb487b
madani6 <anandamadani06@gmail.com>
2019-04-12 10:28:14 +0700

    Install raven-js@3.26.4Logging Errors

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Raven from "raven-js";
 import { toast } from "react-toastify";
 
 axios.interceptors.response.use(null, error => {
@@ -8,8 +9,8 @@ axios.interceptors.response.use(null, error => {
     error.response.status < 500;
 
   if (!expectedError) {
-    console.log("logging the error", error);
-    toast.error("An unexpected error occurrred.");
+    Raven.captureException(error);
+    toast("An unexpected error occurrred.");
   }
 
   return Promise.reject(error);
